5ffe2057f51c4c5814863103725b5fa9f47aefe3
oscarAlejandro <oscaralejandro34@yahoo.com.ar>
2016-11-23 23:49:11 -0300

    agregado de ataque brasas con charmander y Jigllypuff

--- test/fiuba/algo3/model/algomon/BrasasTests.java
+++ test/fiuba/algo3/model/algomon/BrasasTests.java
@@ -23,4 +23,18 @@ public class BrasasTests {
 		assertEquals(142d,squirtle.getPuntosDeVida(),0.000d);
 	}
 	
+	
+	@Test
+	public void test_AtaqueConBrasasRecibeACharmanderComoAtacante_y_A_JiggypuffComoAtacadoYleQuita16PuntosAjigglipuff(){
+		
+		Algomon charmander=new Charmander();
+		Algomon jigglipuff=new Jigglypuff();
+		
+		Ataque brasas=new Brasas();
+		
+		brasas.causarEfecto(jigglipuff, charmander);
+		
+		assertEquals(114d,jigglipuff.getPuntosDeVida(),0.000d);
+		
+	}
 }
