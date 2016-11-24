d5f2992de4573622f2f60727851ebbc6e59935ed
fabiancolque16 <colque.fabian@gmail.com>
2016-11-24 06:46:40 -0300

    Se agrega test04 de ContenedorDeAtaque
    Se agrega test que comprueba el metodo SosDeAtaque de la clase
    ContenedorDeAtaque

--- test/fiuba/algo3/model/ContenedorDeAtaqueTest.java
+++ test/fiuba/algo3/model/ContenedorDeAtaqueTest.java
@@ -1,6 +1,7 @@
 package fiuba.algo3.model;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import java.util.Stack;
 
@@ -45,6 +46,16 @@ public class ContenedorDeAtaqueTest {
 		contenedor.aumentarCantidad(2);
 		assertEquals(contenedor.getCantidadAtaquesDisponibles(),4);		
 				
+	}
+	
+	@Test
+	public void test04SeCreaContenedorDeAtaqueConAtaquesBrasasYSeCompruebaQueElContenedorTengaEsosAtaques(){
+		Brasas brasas = new Brasas();
+		Stack<Ataque> ataques = new Stack<Ataque>();
+		ataques.push(brasas);
+		ContenedorDeAtaque contenedor = new ContenedorDeAtaque(ataques);
+		assertTrue(contenedor.sosDeAtaque(Brasas.class));
+		
 	}
 	
 	
