fef54bba7c1b985377cc0b63af3a595faca16932
oscarAlejandro <oscaralejandro34@yahoo.com.ar>
2016-11-23 23:39:23 -0300

    agregado de tests brasas

---
+++ test/fiuba/algo3/model/algomon/BrasasTests.java
@@ -0,0 +1,26 @@
+package fiuba.algo3.model.algomon;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import fiuba.algo3.model.accion.ataque.Ataque;
+import fiuba.algo3.model.accion.ataque.Brasas;
+
+public class BrasasTests {
+
+	
+	@Test
+	public void test_AtaqueConBrasasRecibeComoAtacanteACharmanderYaSquirtleComoAtacadoYleQuita8PuntosASquitle(){
+		
+		Algomon squirtle=new Squirtle();
+		Algomon charmander=new Charmander();
+		
+		Ataque brasas=new Brasas();
+		
+		brasas.causarEfecto(squirtle, charmander);
+		
+		assertEquals(142d,squirtle.getPuntosDeVida(),0.000d);
+	}
+	
+}
