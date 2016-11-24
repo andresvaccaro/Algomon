704ff1bf1c2901480889fb81fcc4eb1575e8ff38
fabiancolque16 <colque.fabian@gmail.com>
2016-11-24 07:43:45 -0300

    Se agrega test02 de la clase EntrenadorTest
    Se agrega test que comprueba que se agregue un algomon al entrenador

--- test/fiuba/algo3/model/EntrenadorTest.java
+++ test/fiuba/algo3/model/EntrenadorTest.java
@@ -8,6 +8,7 @@ import java.util.List;
 import org.junit.Test;
 
 import fiuba.algo3.model.algomon.Algomon;
+import fiuba.algo3.model.algomon.Charmander;
 
 public class EntrenadorTest {
 
@@ -17,6 +18,15 @@ public class EntrenadorTest {
 		assertEquals(entrenador.getNombre(),"Ash");
 		List<Algomon> listaSinAlgomones = new ArrayList<Algomon>();
 		assertEquals(entrenador.getAlgomones(),listaSinAlgomones);
+		
+	}
+	
+	@Test
+	public void test02SeAgregaAlgomonAlEntrenadorYSeVerificaQueSeAgregue(){
+		Entrenador entrenador = new Entrenador("Ash");
+		Algomon charmander = new Charmander();
+		entrenador.agregarAlgomon(charmander);
+		assertEquals(entrenador.obtenerAlgomonActivo(),charmander);
 		
 	}
 	
