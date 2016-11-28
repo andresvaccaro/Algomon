9f809b4f33f862f52ade5982fcae7d193f53ad1c
oscarAlejandro <oscaralejandro34@yahoo.com.ar>
2016-11-28 11:26:37 -0300

    Test charmander recibe una vitamina y se incrementa sus ataques en 2

--- test/fiuba/algo3/model/algomon/CharmanderTest.java
+++ test/fiuba/algo3/model/algomon/CharmanderTest.java
@@ -4,23 +4,45 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import fiuba.algo3.model.accion.ataque.AtaqueRapido;
+import fiuba.algo3.model.accion.ataque.Brasas;
 import fiuba.algo3.model.accion.ataque.Fogonazo;
+import fiuba.algo3.model.accion.elemento.SuperPocion;
+import fiuba.algo3.model.accion.elemento.Vitamina;
 
 public class CharmanderTest {
 	
 	
 	@Test
-	public void test1_CharmanderAtacaAotroAlgomon(){
+	public void test1_CharmanderRecibeUnaSuperPocionEincrementa40puntoDeVida(){
 		
 		
 		Algomon charmander=new Charmander();
-		Algomon squirtle=new Squirtle();
-		
-		charmander.atacar(squirtle, Fogonazo.class);
-		
-		assertEquals(149d,squirtle.getPuntosDeVida(),0.000d);
 	
+		assertEquals(170d,charmander.getPuntosDeVida(),0.000d);
+		
+		charmander.recibirElemento(new SuperPocion());
+		
+		assertEquals(210d,charmander.getPuntosDeVida(),0.000d);	
 		
 	}
+	
+	@Test
+	public void test2_charmanderRecibeUnaVitaminaYseIncrementaEn_2_laCantidadDeSusAtaques(){
+		
+		Algomon charmander=new Charmander();
+		
+		assertEquals(16,charmander.obtenerCantidadDeAtaquesDisponibles(AtaqueRapido.class));
+		assertEquals(4,charmander.obtenerCantidadDeAtaquesDisponibles(Fogonazo.class));
+		assertEquals(10,charmander.obtenerCantidadDeAtaquesDisponibles(Brasas.class));
+		
+		charmander.recibirElemento(new Vitamina());
+		
+		assertEquals(18,charmander.obtenerCantidadDeAtaquesDisponibles(AtaqueRapido.class));
+		assertEquals(6,charmander.obtenerCantidadDeAtaquesDisponibles(Fogonazo.class));
+		assertEquals(12,charmander.obtenerCantidadDeAtaquesDisponibles(Brasas.class));
+	
+	}
+	
 
 }
